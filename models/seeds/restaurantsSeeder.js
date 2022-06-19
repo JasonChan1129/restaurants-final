6d69b4376078480a2080b4e1a858e1eb380e75af
Jason Chan <jasonchan19981017@hotmail.com>
2022-06-19 15:38:17 +0800

    feat: add seeder data

--- models/seeds/restaurantsSeeder.js
+++ models/seeds/restaurantsSeeder.js
@@ -1,21 +1,36 @@
 const Restaurant = require('../restaurant');
-const restaurantList = require('../../restaurant.json').results;
+const User = require('../user');
+const seederRestaurantList = require('../../models/seeds/restaurant.json').results;
+const seederUserList = require('../../models/seeds/user.json').data;
+const bcrypt = require('bcryptjs');
 
 const db = require('../../config/mongoose');
 
-db.once('open', () => {
-	restaurantList.forEach(restaurant => {
-		Restaurant.create({
-			name: restaurant.name,
-			nameEng: restaurant['name_en'],
-			category: restaurant.category,
-			image: restaurant.image,
-			location: restaurant.location,
-			phone: restaurant.phone,
-			googleMap: restaurant.google_map,
-			rating: restaurant.rating,
-			description: restaurant.description,
-		});
+db.once('open', async () => {
+	seederUserList.forEach(seedUser => {
+		bcrypt
+			.genSalt(10)
+			.then(salt => bcrypt.hash(seedUser.password, salt))
+			.then(hash => {
+				User.create({ email: seedUser.email, password: hash })
+					.then(user => {
+						Promise.all(
+							seederRestaurantList.map(restaurant => {
+								if (seedUser.restaurants.includes(restaurant.id)) {
+									restaurant.userId = user._id;
+									return Restaurant.create({
+										...restaurant,
+										nameEng: restaurant.name_en,
+										googleMap: restaurant.google_map,
+									});
+								}
+							})
+						);
+					})
+					.catch(err => console.log(err));
+			})
+			.catch(err => console.log(err));
 	});
+
 	console.log('done!');
 });
