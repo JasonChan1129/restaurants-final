055783ee91569b05dcab4b234376c0ae43d70377
Jason Chan <jasonchan19981017@hotmail.com>
2022-06-19 10:47:23 +0800

    feat: add register function

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+const User = require('../../models/user');
 
 router.get('/login', (req, res) => {
 	res.render('login');
@@ -11,7 +14,42 @@ router.get('/register', (req, res) => {
 	res.render('register');
 });
 
-router.post('/register', (req, res) => {});
+router.post('/register', (req, res) => {
+	const { username, email, password, confirmPassword } = req.body;
+	const errors = [];
+
+	// if missing fields
+	if (!username || !email || !password || !confirmPassword) {
+		errors.push({ message: 'All fields are required.' });
+	}
+	// if password and confirm password not matched
+	if (password !== confirmPassword) {
+		errors.push({ message: 'Password and confirm password not match' });
+	}
+	if (errors.length) {
+		return res.render('register', { username, email, password, confirmPassword, errors });
+	}
+
+	User.findOne({ email }).then(user => {
+		if (user) {
+			errors.push({ message: 'This email has been registered.' });
+			return res.render('register', { username, email, password, confirmPassword, errors });
+		}
+		// hash password and create user
+		return bcrypt
+			.genSalt(10)
+			.then(salt => bcrypt.hash(password, salt))
+			.then(hash => {
+				return User.create({ username, email, password: hash })
+					.then(user => {
+						console.log('user registered.');
+						return res.redirect('/');
+					})
+					.catch(err => console.log(err));
+			})
+			.catch(err => console.log(err));
+	});
+});
 
 router.get('/logout', (req, res) => {});
 
