7124ae2833da88df5e511731cf4028a61072b5d8
Jason Chan <jasonchan19981017@hotmail.com>
2022-06-19 09:33:57 +0800

    feat: create users route

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const home = require('./modules/home');
 const restaurants = require('./modules/restaurants');
 const apiRequest = require('./modules/apiRequest');
+const users = require('./modules/users');
 
-router.use('/', home);
 router.use('/restaurants', restaurants);
+router.use('/users', users);
 router.use('/google', apiRequest);
+router.use('/', home);
 
 module.exports = router;
