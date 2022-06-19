cfb3549f7bf7a0ab44186105d52c0b8d75d4e470
Jason Chan <jasonchan19981017@hotmail.com>
2022-06-19 12:46:34 +0800

    feat: add facebook login

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,14 @@ const home = require('./modules/home');
 const restaurants = require('./modules/restaurants');
 const apiRequest = require('./modules/apiRequest');
 const users = require('./modules/users');
+const auth = require('./modules/auth');
 
 // auth middleware
 const { authenticator } = require('../middleware/auth');
 
 router.use('/restaurants', authenticator, restaurants);
 router.use('/users', users);
+router.use('/auth', auth);
 router.use('/google', apiRequest);
 router.use('/', authenticator, home);
 
